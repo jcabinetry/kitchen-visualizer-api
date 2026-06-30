5477ead2bfe61c216392d38c3151b592e10289f4
jcabinetry <dusty@jcabinetry.com>
2026-06-29 21:49:53 -0600

    Revert admin session login changes
    Restore admin dashboard to ADMIN_API_TOKEN/X-Admin-Token authentication and remove session login endpoint.

--- api/_lib/cors.js
+++ api/_lib/cors.js
@@ -3,7 +3,7 @@ export function setCorsHeaders(req, res, methods = "GET, POST, OPTIONS") {
 
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", methods);
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Admin-Token, X-Admin-Session");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Admin-Token");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
